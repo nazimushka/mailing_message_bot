155d0de402dfc93d666bc1eeffce889963dae7ac
Gasanov Nazim <nazimushka@gmail.com>
2022-12-27 02:56:31 +0300

    Саппорт-бот

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-API_TOKEN = '5945489327:AAEUEGoIGN4g7WwF02O67jtJ_IH26SgOPBc' # токен от тлеграм бота
-admin = 53798263 # id админа, тобиж твой (взять тут - t.me/userinfobot)
+API_TOKEN = '5945489327:AAEUEGoIGN4g7WwF02O67jtJ_IH26SgOPBc' # токен
+admin = 53798263 # взять тут - t.me/userinfobot
